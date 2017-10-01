1d4c22eac9dafdec521bab449a9d66b89184186c
Brandon Diep <bdzer103@yahoo.com>
2017-10-01 15:23:04 -0700

    Added Search function to Index. Minor change to product array in controller

--- RedDevTeamNames/RedDevTeamNames/Controllers/ProductsController.cs
+++ RedDevTeamNames/RedDevTeamNames/Controllers/ProductsController.cs
@@ -10,11 +10,12 @@ namespace RedDevTeamNames.Controllers
 {
     public class ProductsController : ApiController
     {
-        Product[] products = new[] {
+        Product[] products = new Product[]
+                {
             new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
             new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
             new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
-        };
+                };
 
         public IEnumerable<Product> GetAllProducts()
         {
