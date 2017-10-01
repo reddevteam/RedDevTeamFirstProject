b9cc524a4ccccb9b3a79db8beb6a49777a541503
Caleb <caleb.mccann@bellevuecollege.edu>
2017-10-01 14:56:04 -0700

    uncommented out product controller methods

--- RedDevTeamNames/RedDevTeamNames/Controllers/ProductsController.cs
+++ RedDevTeamNames/RedDevTeamNames/Controllers/ProductsController.cs
@@ -10,25 +10,25 @@ namespace RedDevTeamNames.Controllers
 {
     public class ProductsController : ApiController
     {
-        //int[] products = new[] {
-        //    new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-        //    new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-        //    new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
-        //};
+        Product[] products = new[] {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
 
-        //public IEnumerable<Product> GetAllProducts()
-        //{
-        //    return products;
-        //}
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
 
-        //public IHttpActionResult GetProduct(int id)
-        //{
-        //    var product = products.FirstOrDefault((p) => p.Id == id);
-        //    if (product == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return Ok(product);
-        //}
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
     }
 }
