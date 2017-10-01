e378a9fee0769957d2ee7b918097e31ce254f908
Carson Ngo <c.ngo@outlook.com>
2017-10-01 01:45:06 -0700

    Added Product in Models File

---
+++ RedDevTeamNames/RedDevTeamNames/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RedDevTeamNames.Models
+{
+    public class Product
+    {
+        public int Id
+        {
+            get { return Id; } 
+            set { Id = value; }
+        }
+
+        public string Name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+        public string Category
+        {
+            get { return Category; }
+            set { Category = value; }
+        }
+
+        public decimal Price
+        {
+            get { return Price; }
+            set { Price = value; }
+        }
+    }
+
+}
