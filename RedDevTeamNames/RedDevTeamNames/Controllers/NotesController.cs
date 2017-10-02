885be240ab818a21183fd69a11c9e34b76e5746b
Caleb <caleb.mccann@bellevuecollege.edu>
2017-10-02 11:03:29 -0700

    added notes controller

---
+++ RedDevTeamNames/RedDevTeamNames/Controllers/NotesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RedDevTeamNames.Controllers
+{
+    public class NotesController : ApiController
+    {
+        //Note[] notes = new Note[]
+        //{
+        //    new Note { Id = 1, Priority = 3, Subject = "Wake up", Details = "Set alarm of 7:00 am and get out of bed."},
+        //    new Note { Id = 2, Priority = 2, Subject = "Eat breakfast", Details = "Eat a healthy breakfast."},
+        //    new Note { Id = 3, Priority = 5, Subject = "Go to work", Details = "Get to work before 9:00 am."}
+
+        //};
+
+        //public IEnumerable<Note> GetAllNotes()
+        //{
+        //    return Note;
+        //}
+
+        //public IHttpActionResult GetNote(int id)
+        //{
+        //    var note = notes.FirstOrDefault((p) => p.Id == id);
+        //    if (note == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(note);
+        //}
+    }
+}
