1fbef6835fcbf8820870b42ec586b90cbeefd31e
Caleb McCann <caleb@caleb.com>
2017-10-04 12:00:43 -0700

    package removal and update

--- RedDevTeamNames/RedDevTeamNames/Models/Notes.cs
+++ RedDevTeamNames/RedDevTeamNames/Models/Notes.cs
@@ -7,6 +7,7 @@ namespace RedDevTeamNames.Models
 {
     public class Note
     {
+        [BsonId]
         public int Id { get; set; }
         public string Subject { get; set; }
         public string Details { get; set; }
