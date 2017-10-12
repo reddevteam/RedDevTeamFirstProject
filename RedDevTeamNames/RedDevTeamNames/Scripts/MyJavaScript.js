5e23cb7bb6c6f3ac98298868b3cb968db1e8b4fe
Carson Ngo <c.ngo@outlook.com>
2017-10-12 00:28:07 -0700

    Add css themes, and cleaned index page code: added a Scripts Folder to contain Javascript.
    CSS currently not implemented, but is ready to play with.

---
+++ RedDevTeamNames/RedDevTeamNames/Scripts/MyJavaScript.js
@@ -0,0 +1,81 @@
+﻿     var uri = 'api/notes';
+
+        $(document).ready(function () {
+            // Send an AJAX request
+            refreshNoteList();
+        });
+
+        function formatItem(item) {
+            return item.Priority + ">" + item.Subject + ":  " + item.Details;
+        }
+
+        function refreshNoteList() {
+            $.getJSON(uri)
+                .done(function (data) {
+                    // On success, 'data' contains a list of products.
+                    $.each(data, function (key, item) {
+                        // Add a list item for the product.
+                        $('<li>', { text: formatItem(item) }).appendTo($('#notes'));
+                    });
+                });
+        }
+
+        function clearResponse() {
+            $('#deleteResponse').text("");
+            $('#saveResponse').text("");
+        }
+
+        function find() {
+            clearResponse()
+            var id = $('#noteId').val();
+            $.getJSON(uri + '/' + id)
+                .done(function (data) {
+                    $('#note').text(formatItem(data));
+                })
+                .fail(function (jqXHR, textStatus, err) {
+                    $('#note').text('Error: ' + err);
+                });
+        }
+
+        function deleteNote() {
+            clearResponse()
+            var id = $('#deleteNote').val();
+            $.ajax({
+                url: uri + "/" + id,
+                type: "DELETE",
+                contentType: "application/json",
+                success: function () {
+                    $("#notes").empty();      
+                    refreshNoteList();
+                    $('#deleteResponse').text("Success: Note Deleted");
+                },
+                error: function () {
+                    $('#deleteResponse').text("Error: Delete Failed");
+                }
+            });
+        }
+
+        function saveNote() {
+            clearResponse()
+            var note = {
+                subject: $('#Subject').val(),
+                details: $('#Details').val(),
+                priority: $('#Priority').val()
+            };
+
+            $.ajax({
+                url: uri + "/notes",
+                type: "POST",
+                contentType: "application/json",
+                data: JSON.stringify(note),
+                success: function (data) {
+                    //self.notes.push(data);
+                    $("#notes").empty(); 
+                    refreshNoteList();
+                    $('#saveResponse').text("Success: Saved Note");
+                },
+                error: function () {
+                    $('#saveResponse').text("Error: Save Failed");
+                }
+            });
+        }
