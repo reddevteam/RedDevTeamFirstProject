100d2e9e9bda6261bf008e2649af9679467e9389
Carson Ngo <c.ngo@outlook.com>
2017-10-14 21:45:39 -0700

    Forgot to uncomment something

--- RedDevTeamNames/RedDevTeamNames/Scripts/MyJavaScript.js
+++ RedDevTeamNames/RedDevTeamNames/Scripts/MyJavaScript.js
@@ -29,7 +29,7 @@ function clearResponse() {
 }
 
 function find() {
-   // clearResponse()
+    clearResponse()
     var id = $('#noteId').val();
     $.getJSON(uri + '/' + id)
         .done(function (data) {
