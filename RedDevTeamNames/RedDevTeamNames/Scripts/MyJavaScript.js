e45034c80c36c776ab9d0a4a2ecb681f4fb5328e
Brandon Diep <bdzer103@yahoo.com>
2017-10-15 20:52:53 -0700

    Fixed broken previous commit.

--- RedDevTeamNames/RedDevTeamNames/Scripts/MyJavaScript.js
+++ RedDevTeamNames/RedDevTeamNames/Scripts/MyJavaScript.js
@@ -90,39 +90,3 @@ function saveNote() {
 $(document).on('pagebeforeshow', '#delete-page', function () {
 
 });
-
-$(document).on('pagebeforeshow ', '#pageone', function () {   // see: https://stackoverflow.com/questions/14468659/jquery-mobile-document-ready-vs-page-events
-    var info_view = "";      //string to put HTML in
-    $('#notes').empty();  // since I do this everytime the page is redone, I need to remove existing before apending them all again
-
-
-    $.each(data, function (index, record) {   // make up each li as an <a> to the details-page
-        $('#notes').append('<li><a data-transition="pop" data-parm=' + record.Id + ' href="#details-page">' + record.Priority + ' => ' + record.Subject + '</a></li>');
-    });
-
-    $("#notes").listview('refresh');  // need this so jquery mobile will apply the styling to the newly added li's
-
-    $("a").on("click", function (event) {    // set up an event, if user clicks any, it writes that items data-parm into the 
-        //details page's html so I can get it there
-        var parm = $(this).attr("data-parm");  // passing in the record.Id
-        //do something here with parameter on  details page
-        $("#detailParmHere").html(parm);
-
-    });
-
-
-});
-
-$(document).on('pagebeforeshow', '#details-page', function () {
-
-    var textString = 'fix me';
-    var id = $('#detailParmHere').text();
-
-    $.each(data, function (index, record) {
-        if (id = record.Id) {
-            textString = "Priority: " + record.Priority + " Subject: " + record.Subject + " Details: " + record.Details;
-        }
-    });
-
-    $('#showdata').text(textString);
-});
